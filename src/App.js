6c8c9df6768fd2bcfe80273c4f61147f96a55863
Miriam <miriamhinojo@stilografica.es>
2021-02-12 12:16:38 +0100

    Nivell1-Exercici1-Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import {useState} from "react";
+import axios from "axios";
+
 export default () => {
+    const [joke, setJoke] = useState("");
+    //With Axios
+    const getJoke = () => {
+        axios
+            .get("https://icanhazdadjoke.com/", {
+                headers: {
+                    Accept: "application/json",
+                },
+            })
+            .then((res) => {
+                setJoke(res.data.joke);
+            })
+    }
     return (
         <div>
-            hello World!
+            <h1>Preparat per riure?	&#129315;</h1>
+            <div>{joke}</div>
+            <button onClick={() =>getJoke()}>Següent</button>
         </div>
     );
 };
